34bc463d9b81733fb742dc5e7bb042f972a80f7a
Eugene Goriachkin <egoriachkin@scnsoft.com>
2025-11-17 22:15:34 +0300

    restricted analytics for editors

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { CAppService } from "src/app/services/app.service";
-import { CAuthService } from "src/app/services/auth.service";
-import { CSomePage } from "../some.page";
+import { Component, OnInit } from '@angular/core';
+import { CAppService } from 'src/app/services/app.service';
+import { CAuthService } from 'src/app/services/auth.service';
+import { CSomePage } from '../some.page';
 
 @Component({
-    selector: "home-page",
-    templateUrl: "home.page.html",
-    styleUrls: ["home.page.scss"],
+  selector: 'home-page',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class CHomePage extends CSomePage implements OnInit { 
-    constructor(
-        protected appService: CAppService,
-        protected authService: CAuthService,
-    ) 
-    {
-        super(appService);
-    }  
+export class CHomePage extends CSomePage implements OnInit {
+  isAnalyticsAvailable = this.group_id === 1 || this.group_id === 3;
+  constructor(
+    protected appService: CAppService,
+    protected authService: CAuthService
+  ) {
+    super(appService);
+  }
 
-    get group_id(): number {return this.authService.authData.group_id;}
+  get group_id(): number {
+    return this.authService.authData.group_id;
+  }
 
-    public ngOnInit(): void {        
-        this.appService.setTitle(this.thelang.words["home-head"]); 
-        this.appService.monitorLog("[home] page loaded");         
-    } 
+  public ngOnInit(): void {
+    this.appService.setTitle(this.thelang.words['home-head']);
+    this.appService.monitorLog('[home] page loaded');
+  }
 }
